0e212e367714061ce305e060cda48b2579c4da8e
Daniel Robinson <d.robinson004@gmail.com>
2015-05-25 10:34:27 -0400

    basic styling

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,27 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+
+    <nav id="main-nav" role="navigation" class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+            <img src="<?php echo Roots\Sage\Assets\asset_path('images/logo.png') ?>" width="278" height="70" alt="<?php bloginfo('name'); ?>" />
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="main-nav-collapse">
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-right']);
+          endif;
+          ?>
+        </div>
+      </div>
     </nav>
   </div>
 </header>
