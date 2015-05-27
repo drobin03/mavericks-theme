50c3c63326abd85f5a6e315bb9cfade17c0b3269
Daniel Robinson <d.robinson004@gmail.com>
2015-05-27 09:54:21 -0400

    added footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,19 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="logo-container">
+          <img src="<?php echo Roots\Sage\Assets\asset_path('images/du-footer-logo.png') ?>" width="200" height="108" alt="DeliberateU" />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="below-footer">
+          <?php # dynamic_sidebar('sidebar-footer'); ?>
+          Copyright <?php echo date("Y"); ?> DeliberateU Inc.
+        </div>
+      </div>
+    </div>
   </div>
 </footer>
