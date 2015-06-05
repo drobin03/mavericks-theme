50f9138547efac23630af2539421d55bfc95f7aa
Daniel Robinson <d.robinson004@gmail.com>
2015-06-05 16:44:34 -0400

    first stab at adding custom feed

--- functions.php
+++ functions.php
@@ -28,3 +28,14 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+remove_all_actions( 'do_feed_rss2' );
+add_action( 'do_feed_rss2', 'mavericks_blog_feed_rss2', 10, 1 );
+
+function mavericks_blog_feed_rss2( $for_comments ) {
+    $rss_template = get_template_directory() . '/templates/feeds/mavericks_blog_feed_rss2.php';
+    // if( get_query_var( 'post_type' ) == 'acme_product' and file_exists( $rss_template ) )
+    load_template( $rss_template );
+    // else
+        // do_feed_rss2( $for_comments ); // Call default function
+}
