84b7f76676404fb3eb2930bab0aa101c337ebbfa
Daniel Robinson <d.robinson004@gmail.com>
2015-09-17 08:25:14 -0400

    added an embedded page template to embed latest 3 posts in an iframe, to add to clickfunnels

---
+++ base-template-embedded.php
@@ -0,0 +1,20 @@
+<?php
+
+use Roots\Sage\Config;
+use Roots\Sage\Wrapper;
+
+?>
+
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
+  <?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <main class="main" role="main">
+          <?php include Wrapper\template_path(); ?>
+        </main><!-- /.main -->
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </body>
+</html>
