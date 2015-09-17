fcec500809efab43c47b167bcd895aafd36f1784
Daniel Robinson <d.robinson004@gmail.com>
2015-09-17 09:23:36 -0400

    fixed

--- templates/template-embedded.php
+++ templates/template-embedded.php
@@ -4,19 +4,16 @@
  */
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
-
-<!-- Posts -->
-<?php wp_reset_postdata(); ?>
 <?php $args = array(
     'posts_per_page' => '3',
     'orderby' => 'post_date',
     'order' => 'DESC',
     'post_type' => 'post');
-    $recent_posts = new WP_Query($args);
+    query_posts($args);
 ?>
-<?php while ($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
+<?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
+
+<!-- Posts -->
+<?php wp_reset_postdata(); ?>
